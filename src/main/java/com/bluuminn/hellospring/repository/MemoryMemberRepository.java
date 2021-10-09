70e8e7c7fbad0d6fc4970eb9a2612521b4e297d1
Max Kim <bluuminn@gmail.com>
2021-10-09 19:32:00 +0900

    회원 레포지토리 테스트 작성

--- src/main/java/com/bluuminn/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/bluuminn/hellospring/repository/MemoryMemberRepository.java
@@ -32,4 +32,7 @@ public class MemoryMemberRepository implements MemberRepository {
         return new ArrayList<>(store.values());
     }
 
+    public void clearStore() {
+        store.clear();
+    }
 }
