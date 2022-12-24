35473bdbf031294ccbeb7186e147bece1f00eea2
River Kim <bluuminn@gmail.com>
2022-12-24 14:15:13 +0900

    순수 JDBC

--- src/main/java/com/bluuminn/hellospring/SpringConfig.java
+++ src/main/java/com/bluuminn/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.bluuminn.hellospring;
 
+import com.bluuminn.hellospring.repository.JdbcMemberRepository;
 import com.bluuminn.hellospring.repository.MemberRepository;
-import com.bluuminn.hellospring.repository.MemoryMemberRepository;
 import com.bluuminn.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
