9a7ff23d6e35f307ad600c343cd8b80af2dc0791
River Kim <bluuminn@gmail.com>
2022-12-26 11:53:45 +0900

    Spring Data JPA

--- src/main/java/com/bluuminn/hellospring/SpringConfig.java
+++ src/main/java/com/bluuminn/hellospring/SpringConfig.java
@@ -1,31 +1,28 @@
 package com.bluuminn.hellospring;
 
-import com.bluuminn.hellospring.repository.JpaMemberRepository;
 import com.bluuminn.hellospring.repository.MemberRepository;
 import com.bluuminn.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
